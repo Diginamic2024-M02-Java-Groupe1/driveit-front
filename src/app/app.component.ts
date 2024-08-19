be627c6a104afe2fd5983d2efe416725ff97c2c3
AyoubBenziza <ayoub.cbenziza@gmail.com>
2024-08-19 22:25:41 +0200

    add: Menu Component, Drawer Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
 import {NgxSonnerToaster, toast} from "ngx-sonner";
 import {NavbarComponent} from "@components/navbar/navbar.component";
+import {MenuComponent} from "@components/menu/menu.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgxSonnerToaster, NavbarComponent],
+  imports: [NgxSonnerToaster, NavbarComponent, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
