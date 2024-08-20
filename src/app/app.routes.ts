721f96b31b7405ed62c1cefed1877c5abfc6e953
AyoubBenziza <ayoub.cbenziza@gmail.com>
2024-08-20 17:31:16 +0200

    add: Auth Guard & Token Storage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,19 @@
 import { Routes } from '@angular/router';
 import {AuthComponent} from "@components/auth/auth.component";
-import {AjoutVehicleServiceComponent} from "@components/ajout-vehicle-service/modal/ajout-vehicle-service.component";
 import {ResaVehicleComponent} from "@components/resa-vehicle/resa-vehicle.component";
+import {AuthGuard} from "./guards/auth.guard";
 
 export const routes: Routes = [
-  {
-    path: 'auth',
-    component: AuthComponent,
+  { path: 'auth',
+    component: AuthComponent
   },
   {
-    path: 'ajoutVehicule',
-    component: AjoutVehicleServiceComponent
-  },
+    path: 'reserver-vehicule',
+    component: ResaVehicleComponent,
+    canActivate: [AuthGuard] },
   {
-    path:'reserver-vehicule',
-    component:ResaVehicleComponent
-  },
+    path: '',
+    redirectTo: '/reserver-vehicule',
+    pathMatch: 'full'
+  }
 ];
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+interface LoginResponse {
+  token: string;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +15,15 @@ export class AuthService {
 
   constructor(private http: HttpClient) { }
 
-  login(email: string, password: string) {
-    return this.http.post(`${this.url}/login`, {email, password});
+  login(email: string, password: string): Observable<LoginResponse> {
+    return this.http.post<LoginResponse>(`${this.url}/login`, {email, password});
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+
+  isAuthenticated(): boolean {
+    return !!this.getToken();
   }
 }
