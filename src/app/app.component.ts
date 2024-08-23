73d6046319b676bc5a5bb50014c5986f5c8600ce
AyoubBenziza <ayoub.cbenziza@gmail.com>
2024-08-23 17:31:46 +0200

    Delete: AjoutVehicleService Component from App Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {NgxSonnerToaster} from "ngx-sonner";
-import {AjoutVehicleServiceComponent} from "@components/ajout-vehicle-service/modal/ajout-vehicle-service.component";
 import {NavbarComponent} from "@components/navbar/navbar.component";
 import {MenuComponent} from "@components/menu/menu.component";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
@@ -11,7 +10,7 @@ import {CommonModule} from "@angular/common";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgxSonnerToaster, NavbarComponent, AjoutVehicleServiceComponent, MenuComponent, ReactiveFormsModule, FormsModule, CommonModule],
+  imports: [RouterOutlet, NgxSonnerToaster, NavbarComponent, MenuComponent, ReactiveFormsModule, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
