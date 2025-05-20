29d4f203dae716b9d0278541805304652707e093
Cherhazad <cherhazad.boustani@outlook.fr>
2025-05-20 12:12:45 +0200

    finish list visualisation + fix filter form component

--- src/app/components/vehicle-service/lister-vehicules/lister-vehicules.component.ts
+++ src/app/components/vehicle-service/lister-vehicules/lister-vehicules.component.ts
@@ -5,78 +5,81 @@ import {VehicleService} from "@services/vehicle/vehicle.service";
 import {Vehicle} from "@models/vehicle.model";
 import {NgForOf, NgClass, NgOptimizedImage} from "@angular/common";
 import {Button} from "primeng/button";
+import {Router} from "@angular/router";
+import {FilterFormComponent} from "@components/filter-form/filter-form.component";
 
 @Component({
-  selector: 'app-lister-vehicules',
-  standalone: true,
-  imports: [
-    TableModule,
-    NgForOf,
-    NgClass,
-    NgOptimizedImage,
-    Button
-  ],
-  templateUrl: './lister-vehicules.component.html',
-  styleUrl: './lister-vehicules.component.scss'
+    selector: 'app-lister-vehicules',
+    standalone: true,
+    imports: [
+        TableModule,
+        NgForOf,
+        NgClass,
+        NgOptimizedImage,
+        Button,
+        FilterFormComponent
+    ],
+    templateUrl: './lister-vehicules.component.html',
+    styleUrl: './lister-vehicules.component.scss'
 })
 export class ListerVehiculesComponent implements OnInit {
 
-  vehicles: Vehicle[] = [];
+    vehicles: Vehicle[] = [];
 
-  constructor(private vehicleService: VehicleService) {}
-
-  ngOnInit(): void {
-    this.loadVehicles();
-  }
-
-  loadVehicles(): void {
-    this.vehicleService.getVehicles().subscribe({
-      next: (data: Vehicle[]) => {
-        this.vehicles = data;
-      },
-      error: (error) => {
-        toast.error('Erreur lors du chargement des véhicules');
-        console.error(error);
-      }
-    });
-  }
-
-  isValidUrl(url: string): boolean {
-    try {
-      new URL(url);
-      return true;
-    } catch {
-      return false;
+    constructor(
+        private vehicleService: VehicleService,
+        private router: Router) {
     }
-  }
 
-  onEdit(vehicle: Vehicle): void {
-    if(vehicle.id) {
-        this.vehicleService.updateVehicleService(vehicle.id).subscribe({
-            next: () => {
-            toast.success('Véhicule modifié avec succès');
+    ngOnInit(): void {
+        this.loadVehicles();
+    }
+
+    loadVehicles(): void {
+        this.vehicleService.getVehicles().subscribe({
+            next: (data: Vehicle[]) => {
+                this.vehicles = data;
             },
             error: (error) => {
-            toast.error('Erreur lors de la modification du véhicule');
-            console.error(error);
+                toast.error('Erreur lors du chargement des véhicules');
+                console.error(error);
             }
         });
     }
-  }
 
-    onDelete(vehicle: Vehicle): void {
-      if (vehicle.id) {
-        this.vehicleService.deleteVehicle(vehicle.id).subscribe({
-          next: () => {
-            toast.success('Véhicule supprimé avec succès');
-            this.loadVehicles();
-          },
-          error: (error) => {
-            toast.error('Erreur lors de la suppression du véhicule');
-            console.error(error);
-          }
-        });
-      }
+    isValidUrl(url: string): boolean {
+        try {
+            new URL(url);
+            return true;
+        } catch {
+            return false;
+        }
     }
 
+    onSeeDetails(vehicle: Vehicle): void {
+        //TODO
+    }
+
+    onDelete(vehicle: Vehicle): void {
+        if (vehicle.id) {
+            this.vehicleService.deleteVehicle(vehicle.id).subscribe({
+                next: () => {
+                    toast.success('Véhicule supprimé avec succès');
+                    this.loadVehicles();
+                },
+                error: (error) => {
+                    toast.error('Erreur lors de la suppression du véhicule');
+                    console.error(error);
+                }
+            });
+        }
+    }
+
+    onAddVehicle() {
+        this.router.navigate(['vehicles/add']).then();
+    }
+
+    onFilter(value: any) {
+        console.log(value);
+    }
 }
