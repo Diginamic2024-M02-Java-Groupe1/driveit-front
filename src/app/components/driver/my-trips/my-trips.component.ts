0916b8e63baa2dde05494481c6ec73ec26883bba
PicToWeb <piicronantoine@gmail.com>
2025-05-20 15:24:31 +0200

    feat: component my trip added

---
+++ src/app/components/driver/my-trips/my-trips.component.ts
@@ -0,0 +1,45 @@
+import {Component, OnInit, inject, DestroyRef} from '@angular/core';
+import {FilterField, FilterFormComponent} from "@components/filter-form/filter-form.component";
+import {CarpoolingService} from "@services/carpooling.service";
+import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
+
+@Component({
+  selector: 'app-my-trips',
+  standalone: true,
+  imports: [
+    FilterFormComponent
+  ],
+  templateUrl: './my-trips.component.html',
+  styleUrl: './my-trips.component.scss'
+})
+export class MyTripsComponent implements OnInit {
+
+  private readonly carpoolingService = inject(CarpoolingService);
+  private readonly destroyRef$ = inject(DestroyRef);
+  protected results: any[] = [];
+  protected filterFields: FilterField[] = [
+    {
+      name: 'tripDate',
+      type: 'date',
+      label: 'Date du trajet'
+    }
+  ];
+
+  ngOnInit(): void {
+    this.getMyTrips();
+  }
+
+  getMyTrips(): void {
+    this.carpoolingService.getCarpoolings().pipe(takeUntilDestroyed(this.destroyRef$))
+      .subscribe({
+      next: (data) => {
+        this.results = data;
+        console.log(data);
+      }
+    });
+  }
+
+  onFilterSubmit(formData: any): void {
+    console.log('Données du filtre:', formData);
+  }
+}
